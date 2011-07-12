4cff93637cbbde6ad81bf01a3918ec82b9ac18c7
sakass <rparnpuu@itcollege.ee>
2011-07-12 14:11:48 +0300

    worksLocally

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Testyl::Application.routes.draw do
   #match 'products/:id', :to => 'catalog#view'
   #match ':controller/:action' , :action => "list", :controller => "default"  
  
-  root :to => 'default#index'
-  match '/:controller(/:action(/:id))'
   
+  match '/:controller(/:action(/:id))'
+  root :to => 'default#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
